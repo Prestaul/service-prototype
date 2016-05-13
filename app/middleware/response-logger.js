eb9fa61f7a8925c392cc137a0fd687b88de29f19
Matt Dunlap <matt.dunlap@leankit.com>
2016-05-13 11:19:36 -0600

    Fixed logged url in response-logger middleware

--- app/middleware/response-logger.js
+++ app/middleware/response-logger.js
@@ -2,6 +2,6 @@ import responseTime from "response-time";
 
 export default function( log ) {
 	return responseTime( ( req, res, time ) => {
-		( req.log || log ).info( { res, time }, "Completed request (%s %s) in %d ms", req.method, req.url, time );
+		( req.log || log ).info( { res, time }, "Completed request (%s %s) in %d ms", req.method, req.originalUrl, time );
 	} );
 }
